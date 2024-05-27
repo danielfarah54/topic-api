1a5dd5afd3fbd1fa6af545117447682c6a57f4ad
Daniel Farah <danielfarah54@gmail.com>
2024-05-26 21:57:16 -0300

    fix(topic): fix update permission

--- src/services/topic.service.ts
+++ src/services/topic.service.ts
@@ -1,8 +1,10 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
+import { Topic } from '@prisma/client';
 
 import { UuidInput } from '@/common/dtos/id.dto';
 import { TopicCreateInput, TopicFilterInput, TopicUpdateInput } from '@/common/dtos/topic.dto';
 import { RequestException } from '@/common/exceptions/request-exception.exception';
+import { Paginated } from '@/common/interfaces/paginated.interface';
 import { TopicRepository } from '@/repositories/topic.repository';
 import { NoteService } from '@/services/note.service';
 
@@ -10,34 +12,40 @@ import { NoteService } from '@/services/note.service';
 export class TopicService {
   constructor(private topicRepository: TopicRepository, private readonly noteService: NoteService) {}
 
-  async create(data: TopicCreateInput & { userId: string }) {
+  create(data: TopicCreateInput & { userId: string }): Promise<Topic> {
     return this.topicRepository.create(data);
   }
 
-  async getById(id: string) {
-    const topic = await this.topicRepository.getById(id);
+  async getById(id: string): Promise<Topic> {
+    const topic = await this.topicRepository.getBy({ id });
 
     if (!topic) throw new RequestException('TopicNotFound', HttpStatus.BAD_REQUEST);
 
     return topic;
   }
 
-  async getAll(data?: TopicFilterInput) {
+  getAll(data?: TopicFilterInput): Promise<Paginated<Topic>> {
     return this.topicRepository.getAll(data);
   }
 
-  async update({ id, name }: TopicUpdateInput) {
-    const currentTopic = await this.topicRepository.getById(id);
+  async update(data: TopicUpdateInput & { userId: string }): Promise<Topic> {
+    const topic = await this.topicRepository.getBy({ id: data.id, userId: data.userId });
 
-    if (!currentTopic) {
+    if (!topic) {
       throw new RequestException('TopicNotFound', HttpStatus.BAD_REQUEST);
     }
 
-    return this.topicRepository.update(id, { name });
+    const hasNotes = await this.noteService.getAnyByTopicId(topic.id);
+
+    if (hasNotes) {
+      throw new RequestException('TopicHasNotes', HttpStatus.BAD_REQUEST);
+    }
+
+    return this.topicRepository.update(data.id, { name: data.name });
   }
 
-  async delete(data: UuidInput) {
-    const topic = await this.topicRepository.getById(data.id);
+  async delete(data: UuidInput & { userId: string }): Promise<Topic> {
+    const topic = await this.topicRepository.getBy({ id: data.id, userId: data.userId });
 
     if (!topic) {
       throw new RequestException('TopicNotFound', HttpStatus.BAD_REQUEST);
